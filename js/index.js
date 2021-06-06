6075ad8cf44b95e26b63f435cc636886f5730693
trevorlecroy <trevorlecroy@hotmail.com>
2021-06-06 19:00:14 -0400

    Added a few click events

--- js/index.js
+++ js/index.js
@@ -1,30 +1,49 @@
 // Your code goes here
 
+const body = document.querySelector('body');
 const introIMG = document.querySelector('.intro > img');
-const introIMGHeight = introIMG.style.height;
+let introIMGHeight = introIMG.style.height;
 
 const contentIMGS = document.querySelectorAll('.img-content > img')
 
-introIMG.addEventListener('pointerenter', () => {
+
+const defaultBackgroundColor = body.style.backgroundColor;
+document.addEventListener('keydown', eed => {
+    body.style.backgroundColor = 'red';
+});
+
+document.addEventListener('keyup', e => {
+    body.style.backgroundColor = defaultBackgroundColor;
+})
+
+document.addEventListener('copy', e => { 
+    e.clipboardData.setData('text/plain', 'You cannot copy text from this site');
+    e.preventDefault();
+});
+
+introIMG.addEventListener('pointerenter', e => {
     console.log(introIMG);
     introIMG.style['border-radius'] = "5em";
     introIMG.style.height = "300px";
 });
 
-introIMG.addEventListener('pointerout', () => {
+introIMG.addEventListener('pointerout', e => {
     introIMG.style['border-radius'] = '0';
     introIMG.style.height = introIMGHeight;
 });
 
+introIMG.addEventListener('dblclick', e => {
+    introIMGHeight = '300px';
+});
 
 let selectedIMG = null;
 contentIMGS.forEach((img) => {
-    img.addEventListener('drag', () => {
+    img.addEventListener('drag', e => {
         img.style.filter = 'blur(2px)';
         selectedIMG      = img.src;
     });
 
-    img.addEventListener('dragend', () => {
+    img.addEventListener('dragend', e => {
         img.style.filter   = 'blur(0px)';
     });
 });
