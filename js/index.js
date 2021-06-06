90febb782775729170892d9558d5f464c08b3e2d
trevorlecroy <trevorlecroy@hotmail.com>
2021-06-06 19:13:32 -0400

    Finished final events

--- js/index.js
+++ js/index.js
@@ -9,15 +9,22 @@ const contentIMGS = document.querySelectorAll('.img-content > img')
 
 const defaultBackgroundColor = body.style.backgroundColor;
 document.addEventListener('keydown', eed => {
-    body.style.backgroundColor = 'red';
+    body.style.backgroundColor = 'blue';
 });
 
 document.addEventListener('keyup', e => {
     body.style.backgroundColor = defaultBackgroundColor;
 })
 
+document.addEventListener('cut', e => {
+    e.clipboardData.setData('text/plain', 'You cannot cut text from this site');
+    body.style.backgroundColor = 'red'; 
+    e.preventDefault();
+});
+
 document.addEventListener('copy', e => { 
     e.clipboardData.setData('text/plain', 'You cannot copy text from this site');
+    body.style.backgroundColor = 'red'; 
     e.preventDefault();
 });
 
@@ -41,6 +48,15 @@ contentIMGS.forEach((img) => {
     img.addEventListener('drag', e => {
         img.style.filter = 'blur(2px)';
         selectedIMG      = img.src;
+    });
+
+    img.addEventListener('wheel', e => {
+        if(e.deltaY < 0) {
+            e.target.style.transform = 'scale(' + e.deltaY * -0.05 + ')';
+        } else if(e.deltaY > 0) {
+            e.target.style.transform = 'scale(1)';
+        } 
+        e.preventDefault();
     });
 
     img.addEventListener('dragend', e => {
